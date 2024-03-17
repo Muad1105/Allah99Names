8e8ac3c573db3feeb0794a32a926826578069960
muad1105 <muadnoushad@gmail.com>
2024-03-17 11:37:37 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { store } from "./redux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="Allah99Names">
       <Provider store={store}>
         <App />
       </Provider>
